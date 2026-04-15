6091b42eba964d91448d05bbf8035b12e7f6cde8
patelurvisha57-max <patelurvisha57@gmail.com>
2026-04-15 14:36:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,18 @@
-const CACHE_NAME = 'codegen-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
-];
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Caching Files');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+const CACHE_NAME = "codegen-v1";
 
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
- 
-  return self.clients.claim();
 });
-self.addEventListener('fetch', (event) => {
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-  
-        return response;
-      })
-      .catch(() => {
-       return caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-          
-          return new Response('Offline content not available', {
-            status: 404,
-            statusText: 'Offline',
-            headers: { 'Content-Type': 'text/plain' }
-          });
-        });
-      })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
